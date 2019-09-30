500328d1764100547dc1454c8dbfa8262acfadd7
lunaluxe <lunaluxe@icloud.com>
2019-09-29 19:56:22 -0500

    World Design Changes

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,7 +3,7 @@
 public class RubyController : MonoBehaviour
 {
     Vector2 position; //Vector 2 is a representation of 2D vectors and points, used to represent 2D positions of (our character's) X and Y 
-    void Update() //Update() runs every frame - useful for  input compared to FixedUpdate.
+    void Update() //Update() runs every frame - useful for input compared to FixedUpdate.
     {
         position.x += (Input.GetAxis("x") * 3.0f * Time.deltaTime); //Input ensures position.x, position.y movements are mapped to w, a, s, d and the arrow keys.
         position.y += (Input.GetAxis("y") * 3.0f * Time.deltaTime); // Axes renamed in project settings to x and y for consistency with positioning
